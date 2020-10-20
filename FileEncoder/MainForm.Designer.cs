8638a8af3d738a156822a0e3bba3710fd4daa598
NatanM <natanaelmarian@hotmail.com>
2020-10-20 16:36:39 -0300

    Adicionar arquivos de projeto.

---
+++ FileEncoder/MainForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FileEncoder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncodeButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EncodeButton
+            // 
+            this.EncodeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EncodeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EncodeButton.Location = new System.Drawing.Point(12, 12);
+            this.EncodeButton.Name = "EncodeButton";
+            this.EncodeButton.Size = new System.Drawing.Size(106, 37);
+            this.EncodeButton.TabIndex = 0;
+            this.EncodeButton.Text = "Encode";
+            this.EncodeButton.UseVisualStyleBackColor = true;
+            this.EncodeButton.Click += new System.EventHandler(this.EncodeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(339, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Decode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 308);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EncodeButton);
+            this.Name = "MainForm";
+            this.Text = "File Encoder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EncodeButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FileEncoder/MainForm.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileEncoder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EncodeButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+                Encode(new FileInfo(dialog.FileName));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Filter = "Encoded files|*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                Decode(new FileInfo(dialog.FileName));
+        }
+
+        private void Decode(FileInfo file)
+        {
+            Bitmap image = Bitmap.FromFile(file.FullName, false) as Bitmap;
+
+            List<byte> header = new List<byte>();
+            List<byte> body = new List<byte>();
+
+            string filename = "";
+
+            bool finished = false;
+            for(int x=0; x < image.Width; x++)
+            {
+                if (finished)
+                    break;
+                for(int y=0; y < image.Height; y++)
+                {
+                    Color cByte = image.GetPixel(x, y);
+                    if (string.IsNullOrEmpty(filename))
+                    {
+                        if(cByte.A == 240)
+                        {
+                            if(cByte.R != 255)
+                                header.Add(cByte.R);
+                            if (cByte.G != 255)
+                                header.Add(cByte.G);
+                            if (cByte.B != 255)
+                                header.Add(cByte.B);
+                            filename = Encoding.UTF8.GetString(header.ToArray());
+                            Debug.WriteLine(filename);
+                            continue;
+                        }
+                        header.Add(cByte.R);
+                        header.Add(cByte.G);
+                        header.Add(cByte.B);
+                    } else
+                    {
+                        if(cByte.A == 1)
+                        {
+                            if (cByte.R != 255)
+                                body.Add(cByte.R);
+                            if (cByte.G != 255)
+                                body.Add(cByte.G);
+                            if (cByte.B != 255)
+                                body.Add(cByte.B);
+                            finished = true;
+                            break;
+                        }
+                        body.Add(cByte.R);
+                        body.Add(cByte.G);
+                        body.Add(cByte.B);
+                    }
+                }
+            }
+
+            image.Dispose();
+            string ext = filename.Split('.')[filename.Split('.').Length - 1];
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = $"Decoded File|*.{ext}";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                File.WriteAllBytes(dialog.FileName, body.ToArray());
+        }
+
+        private void SetByteFinish(List<Color> bytes, List<byte> rgb)
+        {
+            if (rgb.Count > 0)
+            {
+                int r = 255, g = 255, b = 255;
+
+                try { r = rgb[0]; g = rgb[1]; b = rgb[2]; } catch (Exception ex) { }
+                bytes.Add(Color.FromArgb(240, r, g, b));
+            }
+            if(bytes[bytes.Count-1].A != 240)
+                bytes.Add(Color.FromArgb(240, 255, 255, 255));
+        }
+        private void SetFileFinish(List<Color> bytes, List<byte> rgb)
+        {
+            if (rgb.Count > 0)
+            {
+                int r = 255, g = 255, b = 255;
+
+                try { r = rgb[0]; g = rgb[1]; b = rgb[2]; } catch (Exception ex) { }
+                bytes.Add(Color.FromArgb(1, r, g, b));
+            }
+            if (bytes[bytes.Count - 1].A != 1)
+                bytes.Add(Color.FromArgb(1, 255, 255, 255));
+        }
+
+        private void Encode(FileInfo file)
+        {
+            byte[] fileName = Encoding.UTF8.GetBytes(file.Name);
+            byte[] fileData = File.ReadAllBytes(file.FullName);
+            List<Color> encodedBytes = new List<Color>(fileData.Length);
+            
+            List<byte> rgb = new List<byte>();
+            
+            for(int i=0; i < fileName.Length;i++)
+            {
+                rgb.Add(fileName[i]);
+                if(rgb.Count >= 3)
+                {
+                    encodedBytes.Add(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+                    rgb.Clear();
+                }
+            }
+            SetByteFinish(encodedBytes, rgb);
+            for(int i=0; i < fileData.Length; i++)
+            {
+                rgb.Add(fileData[i]);
+                if (rgb.Count >= 3)
+                {
+                    encodedBytes.Add(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+                    rgb.Clear();
+                }
+            }
+            SetFileFinish(encodedBytes, rgb);
+
+            int total = Convert.ToInt32(Math.Sqrt(Convert.ToDouble(encodedBytes.Count)));
+
+            Bitmap bmp = new Bitmap(total, total+1, PixelFormat.Format32bppArgb);
+            bmp.MakeTransparent();
+
+            int location = 0;
+            for(int x=0; x < bmp.Width;x++)
+            {
+                if (location >= encodedBytes.Count)
+                    break;
+                for(int y=0; y < bmp.Height;y++)
+                {
+                    if (location >= encodedBytes.Count)
+                        break;
+                    bmp.SetPixel(x, y, encodedBytes[location]);
+                    location++;
+                }
+            }
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Encoded File|*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                bmp.Save(dialog.FileName, ImageFormat.Png);
+
+            bmp.Dispose();
+        }
+    }
+}
